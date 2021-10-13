f587a72f8819f5a65945ff4d9814ce59feb0e5ae
HARISH BODDULA <HARISH BODDULA@LAPTOP-BH41G14T>
2021-10-13 16:20:07 +0530

    updated user1

---
+++ Ostriches/src/test/java/testScript/Temp.java
@@ -0,0 +1,14 @@
+package testScript;
+
+import org.testng.annotations.Test;
+
+public class Temp {
+
+	@Test
+	public void temp(){
+		// TODO Auto-generated method stub
+
+		System.out.println("Hiii");
+	}
+
+}
